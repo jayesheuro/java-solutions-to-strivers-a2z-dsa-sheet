a8eeaf9f67a1573bf3f70346680763bb8a3b503d
Jayesh <jayehssingh001@gmail.com>
2025-07-20 11:03:22 +0530

    solve pattern 2,3,4,5,6

--- learn_the_basics/lec_2/Patterns.java
+++ learn_the_basics/lec_2/Patterns.java
@@ -1,7 +1,7 @@
 package learn_the_basics.lec_2;
 
 public class Patterns {
-    void pattern1(int n) {
+    public void pattern1(int n) {
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < n; j++) {
                 System.out.print("*");
@@ -11,6 +11,56 @@ public class Patterns {
         }
     }
 
+    public void pattern2(int n) {
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+//            or just System.out.println();
+        }
+    }
+
+
+    public void pattern3(int n) {
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j <= i; j++) {
+                System.out.print(j+1);
+            }
+            System.out.print("\n");
+//            or just System.out.println();
+        }
+    }
+
+
+    public void pattern4(int n) {
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j <= i; j++) {
+                System.out.print(i);
+            }
+            System.out.print("\n");
+//            or just System.out.println();
+        }
+    }
+
+    public void pattern5(int n) {
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n - i; j++) {
+                System.out.print('*');
+            }
+            System.out.print("\n");
+//            or just System.out.println();
+        }
+    }
+    public void pattern6(int n) {
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n - i; j++) {
+                System.out.print(j+1);
+            }
+            System.out.print("\n");
+//            or just System.out.println();
+        }
+    }
     public static void main(String[] args) {
         Patterns p = new Patterns();
 
@@ -21,5 +71,45 @@ public class Patterns {
 //        *****
 //        *****
         p.pattern1(5);
+
+//        2.
+//        *
+//        **
+//        ***
+//        ****
+//        *****
+        p.pattern2(5);
+
+//        3.
+//        1
+//        12
+//        123
+//        1234
+//        12345
+        p.pattern3(5);
+
+//        4.
+//        1
+//        22
+//        333
+//        4444
+//        55555
+        p.pattern4(5);
+
+//        5.
+//        *****
+//        ****
+//        ***
+//        **
+//        *
+        p.pattern5(5);
+
+//        6.
+//        12345
+//        1234
+//        123
+//        12
+//        1
+        p.pattern6(5);
     }
 }
