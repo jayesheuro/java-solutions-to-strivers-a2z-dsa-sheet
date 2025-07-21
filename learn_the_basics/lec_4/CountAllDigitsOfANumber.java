150c13ea799a8add57c11321affca173354cd25c
Jayesh <jayehssingh001@gmail.com>
2025-07-21 23:33:43 +0530

    fix countNumberOfDigits

--- learn_the_basics/lec_4/CountAllDigitsOfANumber.java
+++ learn_the_basics/lec_4/CountAllDigitsOfANumber.java
@@ -2,13 +2,16 @@ package learn_the_basics.lec_4;
 
 public class CountAllDigitsOfANumber {
     public int countNumberOfDigits(int n) {
-        int count = 0;
+        n = Math.abs(n);
 
-        while (n / 10 != 0) {
+        if (n == 0) return 1;
+
+        int count = 0;
+        while (n != 0) {
             n = n / 10;
             count++;
         }
-        return count + 1;
+        return count;
     }
 
     public static void main(String[] args) {
