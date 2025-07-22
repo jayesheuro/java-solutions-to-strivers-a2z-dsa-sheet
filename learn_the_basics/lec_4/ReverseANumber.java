ea861215b2d712b1abd0eb37dac90011a847199a
Jayesh <jayehssingh001@gmail.com>
2025-07-22 10:38:06 +0530

    solve reversANumber

---
+++ learn_the_basics/lec_4/ReverseANumber.java
@@ -0,0 +1,21 @@
+package learn_the_basics.lec_4;
+
+public class ReverseANumber {
+    public int reverseANumber(int n) {
+        if (n == 0) return 0;
+        int rev = 0;
+
+        while(n != 0) {
+            int rem = n % 10;
+            rev = rev * 10 + rem;
+            n /= 10;
+        }
+        return rev;
+    }
+
+    public static void main(String[] args) {
+        ReverseANumber c = new ReverseANumber();
+        System.out.println(c.reverseANumber( -12));
+//        System.out.println( -0123 % 10);
+    }
+}
