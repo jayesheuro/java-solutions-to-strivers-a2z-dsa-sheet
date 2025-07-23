c325bb2f18c8f0030a678b95bb84dfad328df864
Jayesh <jayehssingh001@gmail.com>
2025-07-23 18:54:19 +0530

    solve sorted divisors and check for prime

---
+++ learn_the_basics/lec_4/CheckForPrime.java
@@ -0,0 +1,20 @@
+package learn_the_basics.lec_4;
+
+public class CheckForPrime {
+    public boolean check (int n) {
+        boolean flag = true;
+        for (int i = 2; i <= n/2; i++) {
+//            System.out.println("Loop ran for " + (i-1) + " times");
+            if (n % i == 0) {
+                flag = false;
+                break;
+            }
+        }
+        return flag;
+    }
+
+    public static void main(String[] args) {
+        CheckForPrime c = new CheckForPrime();
+        System.out.println(c.check(23));
+    }
+}
---
+++ learn_the_basics/lec_4/SortedDivisors.java
@@ -0,0 +1,21 @@
+package learn_the_basics.lec_4;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SortedDivisors {
+    public List<Integer> sortedDivisors (int n) {
+        List<Integer> list = new ArrayList<>();
+            for(int i = 1; i <= n; i++) {
+                if (n % i == 0) {
+                    list.add(i);
+                }
+            }
+        return list;
+    }
+
+    public static void main(String[] args) {
+        SortedDivisors sd = new SortedDivisors();
+        System.out.println(sd.sortedDivisors(12));
+    }
+}
