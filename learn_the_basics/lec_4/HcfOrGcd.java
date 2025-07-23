6ffdddb6cbb138f51a065cad10df93f9e655d6fb
Jayesh <jayehssingh001@gmail.com>
2025-07-23 17:51:39 +0530

    optimize hcf gcd

--- learn_the_basics/lec_4/HcfOrGcd.java
+++ learn_the_basics/lec_4/HcfOrGcd.java
@@ -2,12 +2,27 @@ package learn_the_basics.lec_4;
 
 public class HcfOrGcd {
     public int hcf (int n1 ,int n2) {
-        int min = Math.min(n1, n2);
-        int hcf = 1;
-        for (int i = 1; i <= min; i++) {
-            hcf = (n1 % i == 0 && n2 % i == 0) ? i : hcf;
+        if (n1 <= 0 || n2 <= 0){
+            return -1;
         }
-        return hcf;
+        // less optimized O(n)
+//        if (n1 == n2){
+//            return n1;
+//        }
+//        int min = Math.min(n1, n2);
+//        int hcf = 1;
+//        for (int i = 1; i <= min; i++) {
+//            hcf = (n1 % i == 0 && n2 % i == 0) ? i : hcf;
+//        }
+//        return hcf;
+
+//        Euclid's algo - The HCF of two numbers a and b is the same as the HCF of b and a % b
+        while(n2 != 0){
+            int temp = n2;
+            n2 = n1 % n2;
+            n1 = temp;
+        }
+        return n1;
     }
 
     public static void main(String[] args) {
