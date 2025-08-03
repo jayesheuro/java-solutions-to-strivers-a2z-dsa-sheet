dd8dadd675aaae643ff6c4e6cd78e771ddb2a789
Jayesh <jayehssingh001@gmail.com>
2025-08-03 06:59:15 +0530

    solve print 1 to n recursively

---
+++ learn_the_basics/lec_5/Print1ToN.java
@@ -0,0 +1,16 @@
+package learn_the_basics.lec_5;
+
+public class Print1ToN {
+    void print1ToN(int n) {
+        if(n == 0) {
+            return;
+        }
+        print1ToN(n - 1);
+        System.out.println(n);
+    }
+
+    public static void main(String[] args) {
+        Print1ToN p = new Print1ToN();
+        p.print1ToN(100);
+    }
+}
