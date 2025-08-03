ff3bf97eea6c4f11ef7270d8f09032bd810f8cac
Jayesh <jayehssingh001@gmail.com>
2025-08-03 07:39:03 +0530

    solve sum of first n numbers recursively

---
+++ learn_the_basics/lec_5/SumOfFirstNNumbers.java
@@ -0,0 +1,16 @@
+package learn_the_basics.lec_5;
+
+public class SumOfFirstNNumbers {
+
+    int calculateSumRecursively(int n) {
+        if (n == 0) return 0;
+        int sum = n + calculateSumRecursively(n - 1);
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        SumOfFirstNNumbers s = new SumOfFirstNNumbers();
+        int sum = s.calculateSumRecursively(100);
+        System.out.println(sum);
+    }
+}
