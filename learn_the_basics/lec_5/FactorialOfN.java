25d2d1cc6761391e99a7e0cc5fa7e7b4f6dc73e8
Jayesh <jayehssingh001@gmail.com>
2025-08-03 07:44:44 +0530

    solve factorial of n recursively

---
+++ learn_the_basics/lec_5/FactorialOfN.java
@@ -0,0 +1,13 @@
+package learn_the_basics.lec_5;
+
+public class FactorialOfN {
+    int calculateFactorialRecursively(int n) {
+        if (n == 1) return 1;
+        return n * calculateFactorialRecursively(n - 1);
+    }
+
+    public static void main(String[] args) {
+        FactorialOfN f = new FactorialOfN();
+        System.out.println(f.calculateFactorialRecursively(5));
+    }
+}
