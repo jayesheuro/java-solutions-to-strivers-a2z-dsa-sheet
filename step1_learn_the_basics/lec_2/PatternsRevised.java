107b44591daf6d1def5b93b25acfeb40e2b0b0f2
Jayesh <jayehssingh001@gmail.com>
2026-03-09 09:47:12 +0530

    revisit pattern 2

--- step1_learn_the_basics/lec_2/PatternsRevised.java
+++ step1_learn_the_basics/lec_2/PatternsRevised.java
@@ -15,7 +15,23 @@ public class PatternsRevised {
             System.out.println();
         }
     }
-    public static void main(String[] args) {
-           Pattern1(5);
+
+    static void Pattern2(int n) {
+        // *
+        // **
+        // ***
+        // ****
+        // *****
+
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
+    public static void main(String[] args) {
+        //    Pattern1(5);
+            Pattern2(5);
+        }
 }
