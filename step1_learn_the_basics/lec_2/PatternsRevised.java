cf63c0924fd32ab9ed0d272f8b15f12241be2659
Jayesh <jayehssingh001@gmail.com>
2026-03-09 10:16:10 +0530

    revisit pattern 7

--- step1_learn_the_basics/lec_2/PatternsRevised.java
+++ step1_learn_the_basics/lec_2/PatternsRevised.java
@@ -91,12 +91,31 @@ public class PatternsRevised {
         }
     }
 
+    static void pattern7(int n) {
+        //     *
+        //    ***
+        //   *****
+        //  *******
+        // *********
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= (n-i); j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= (2*i-1); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
         // pattern1(5);
         // pattern2(5);
         // pattern3(5);
         // pattern4(5);
         // pattern5(5);
-        pattern6(5);
+        // pattern6(5);
+        pattern7(5);
     }
 }
