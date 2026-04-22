cf35819dad9eff015d375f8152a6da4319972e9f
Jayesh Singh <jayeshsingh001@gmail.com>
2026-04-22 07:29:30 +0530

    revise insertion sort

---
+++ step2_sorting/lec_1_sorting_1/revision/InsertionSort.java
@@ -0,0 +1,28 @@
+package step2_sorting.lec_1_sorting_1.revision;
+
+public class InsertionSort {
+    static void insertionSort(int[] arr) {
+        if(arr.length <= 1) return;
+
+        // we consider the first number is always sorted, 
+        // hence our first number to find correct position (key) becomes index 1 (second number)
+        for(int i = 1; i <= arr.length - 1; i++) {
+            int key = arr[i];
+            // now we have to find its correct position in the left part of array
+            int j = i - 1;
+            while(j >= 0 && arr[j] > key) {
+                arr[j+1] = arr[j]; //shift one place to right
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+        
+    }
+    public static void main(String[] args) {
+        int[] arr = {5,3,8,2};
+        insertionSort(arr);
+        for(int i: arr){
+            System.out.print(i + " ");
+        }
+    }
+}
