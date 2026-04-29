7aa9e7f7f05db16171940cc98361ae5c5953cb7e
Jayesh Singh <jayeshsingh001@gmail.com>
2026-04-29 09:00:43 +0530

    solve insertion sort recursive

--- step2_sorting/lec_1_sorting_2/InsertionSortRecursive.java
+++ step2_sorting/lec_1_sorting_2/InsertionSortRecursive.java
@@ -1,5 +1,39 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 public class InsertionSortRecursive {
-    public static void main(String[] args) {
-        
+    static void isr(List<Integer> arr, int n, int i) {
+        if(i == n) return;
+        int j = i;
+        while(j > 0 && arr.get(j) < arr.get(j - 1)) {
+            int temp = arr.get(j);
+            arr.set(j, arr.get(j-1));
+            arr.set(j - 1, temp);
+            j--;
+        }
+        isr(arr, n, i + 1);
+
+        // basic iterative flow
+        // for(int i = 1; i < n; i++) {
+        //     int j = i;
+        //     while(j > 0 && arr.get(j) < arr.get(j - 1)) {
+        //         int temp = arr.get(j);
+        //         arr.set(j, arr.get(j-1));
+        //         arr.set(j - 1, temp);
+        //         j--;
+        //     }
+        // }
     }
+
+    static void insertionSortRecursive(List<Integer> arr) {
+        int n = arr.size();
+        if(n == 1) return;
+        isr(arr, n, 1);
+    }
+
+    public static void main(String[] args) {
+        List<Integer> arr = new ArrayList<>(Arrays.asList(14, 9, 15, 12, 6, 8, 13));
+        insertionSortRecursive(arr);
+        System.out.println(arr);
+    }   
 }
