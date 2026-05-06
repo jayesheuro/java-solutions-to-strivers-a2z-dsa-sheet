c0142b71d3974dffede6706d07ac937b51831f15
Jayesh Singh <jayeshsingh001@gmail.com>
2026-05-06 23:22:38 +0530

    solve remove duplicates from sorted array brute on2

---
+++ step3_arrays/lec_1_easy/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,34 @@
+package step3_arrays.lec_1_easy;
+
+public class RemoveDuplicatesFromSortedArray {
+
+    static int removeDuplicates(int[] arr) {
+        int size = arr.length;
+        if (size == 1)
+            return 1;
+        int uniques = 1;
+        for (int i = 0; i < size; i++) {
+            for (int j = i + 1; j <= size; j++) {
+                if (j == size) {
+                    return uniques;
+                }
+                if (arr[j] > arr[i] && (j - i) > 1) {
+                    int temp = arr[i + 1];
+                    arr[i + 1] = arr[j];
+                    arr[j] = temp;
+                    uniques++;
+                    break;
+                }
+            }
+        }
+        return uniques;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 1, 2, 2, 2, 3, 4, 4, 7, 7, 7, 8, 8, 9, 20 };
+        int uniques = removeDuplicates(arr);
+        for (int i = 0; i < uniques; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
