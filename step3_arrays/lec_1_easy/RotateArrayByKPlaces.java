ebce8c8645b468d5094c0549c898106879578713
Jayesh Singh <jayeshsingh001@gmail.com>
2026-05-09 21:24:22 +0530

    rotate array by k by brute

--- step3_arrays/lec_1_easy/RotateArrayByKPlaces.java
+++ step3_arrays/lec_1_easy/RotateArrayByKPlaces.java
@@ -21,6 +21,29 @@ public class RotateArrayByKPlaces {
         reverse(arr, 0, arr.length - 1);
     }
 
+    static void rotateArrayBrute(int[] arr, int k) {
+        int n = arr.length;
+        k = k % n;
+        if (k == 0 || arr.length == 1)
+            return;
+
+        // Step 1: Copy the first k elements to a temp array
+        int[] temp = new int[k];
+        for (int i = 0; i < k; i++) {
+            temp[i] = arr[i];
+        }
+
+        // Step 2: Shift the rest of the array to the left
+        for (int i = k; i < n; i++) {
+            arr[i - k] = arr[i];
+        }
+
+        // Step 3: Put the temp elements at the end of the original array
+        for (int i = 0; i < k; i++) {
+            arr[n - k + i] = temp[i];
+        }
+    }
+
     public static void main(String[] args) {
         int[] arr = { 1, 2, 3, 4, 5 };
 
@@ -29,7 +52,8 @@ public class RotateArrayByKPlaces {
             System.out.print(i + " ");
         }
 
-        rotateArrayLeft(arr, 2);
+        // rotateArrayLeft(arr, 2);
+        rotateArrayBrute(arr, 2);
         System.out.print("OUTPUT: ");
         for (int i : arr) {
             System.out.print(i + " ");
