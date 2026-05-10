fd21e822079afb4b9a6471ff9f6b20237222449f
Jayesh Singh <jayeshsingh001@gmail.com>
2026-05-10 23:17:28 +0530

    solve union of two sorted arrays using map

--- step3_arrays/lec_1_easy/UnionOfTwoSortedArrays.java
+++ step3_arrays/lec_1_easy/UnionOfTwoSortedArrays.java
@@ -1,18 +1,36 @@
 package step3_arrays.lec_1_easy;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
 
 public class UnionOfTwoSortedArrays {
     static int[] union(int a1[], int a2[]) {
-        Set<Integer> s = new TreeSet<>();
+
+        // // Using TreeSet - O((n1+n2)log(n1+n2)) ~ O(NlogN) where N is the union size
+        // Set<Integer> s = new TreeSet<>();
+        // for (int i : a1) {
+        // s.add(i);
+        // }
+        // for (int i : a2) {
+        // s.add(i);
+        // }
+        // int[] u = s.stream().mapToInt(i -> i).toArray();
+        // return u;
+
+        // Using a HashMap - O(N log N) same as set approach
+        Map<Integer, Integer> m = new HashMap<>();
         for (int i : a1) {
-            s.add(i);
+            m.put(i, 1); // value doesn't really matter
         }
         for (int i : a2) {
-            s.add(i);
+            m.put(i, 1);
         }
+        Set<Integer> s = m.keySet();
         int[] u = s.stream().mapToInt(i -> i).toArray();
+        Arrays.sort(u);
         return u;
     }
 
