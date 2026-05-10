4c20b01ca77dee2fe80269c8cab29d9e3afa18ad
Jayesh Singh <jayeshsingh001@gmail.com>
2026-05-10 23:41:53 +0530

    solve union of two sorted arrays optimally by two pointer

--- step3_arrays/lec_1_easy/UnionOfTwoSortedArrays.java
+++ step3_arrays/lec_1_easy/UnionOfTwoSortedArrays.java
@@ -1,7 +1,9 @@
 package step3_arrays.lec_1_easy;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
@@ -20,18 +22,50 @@ public class UnionOfTwoSortedArrays {
         // int[] u = s.stream().mapToInt(i -> i).toArray();
         // return u;
 
-        // Using a HashMap - O(N log N) same as set approach
-        Map<Integer, Integer> m = new HashMap<>();
-        for (int i : a1) {
-            m.put(i, 1); // value doesn't really matter
+        // // Using a HashMap - O(N log N) same as set approach
+        // Map<Integer, Integer> m = new HashMap<>();
+        // for (int i : a1) {
+        // m.put(i, 1); // value doesn't really matter
+        // }
+        // for (int i : a2) {
+        // m.put(i, 1);
+        // }
+        // Set<Integer> s = m.keySet();
+        // int[] u = s.stream().mapToInt(i -> i).toArray();
+        // Arrays.sort(u);
+        // return u;
+
+        List<Integer> u = new ArrayList<>();
+        int i = 0, j = 0;
+        while (i < a1.length && j < a2.length) {
+            if (a1[i] <= a2[j]) {
+                if (u.isEmpty() || u.get(u.size() - 1) != a1[i]) {
+                    u.add(a1[i]);
+                }
+                i++;
+            } else {
+                if (u.isEmpty() || u.get(u.size() - 1) != a2[j]) {
+                    u.add(a2[j]);
+                }
+                j++;
+            }
+
         }
-        for (int i : a2) {
-            m.put(i, 1);
+        while (j < a2.length) {
+            if (u.isEmpty() || u.get(u.size() - 1) != a2[j]) {
+                u.add(a2[j]);
+            }
+            j++;
         }
-        Set<Integer> s = m.keySet();
-        int[] u = s.stream().mapToInt(i -> i).toArray();
-        Arrays.sort(u);
-        return u;
+
+        while (i < a1.length) {
+            if (u.isEmpty() || u.get(u.size() - 1) != a1[i]) {
+                u.add(a1[i]);
+            }
+            i++;
+        }
+        int[] ua = u.stream().mapToInt(n -> n).toArray();
+        return ua;
     }
 
     public static void main(String[] args) {
