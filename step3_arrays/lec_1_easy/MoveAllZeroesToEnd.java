a0d109a7ecd0b3384e32ff503b04c3924f304299
Jayesh Singh <jayeshsingh001@gmail.com>
2026-05-10 13:54:54 +0530

    solve move all zeroes to end optimal

--- step3_arrays/lec_1_easy/MoveAllZeroesToEnd.java
+++ step3_arrays/lec_1_easy/MoveAllZeroesToEnd.java
@@ -9,15 +9,34 @@ public class MoveAllZeroesToEnd {
         if (n <= 1)
             return;
 
-        int[] arr2 = Arrays.stream(arr).filter(i -> i != 0).toArray();
-        for (int i = 0; i < n; i++) {
-            if (i >= arr2.length) {
-                arr[i] = 0;
-            } else {
-                arr[i] = arr2[i];
+        // BRUTE - use O(n) extra space
+        // int[] arr2 = Arrays.stream(arr).filter(i -> i != 0).toArray();
+        // for (int i = 0; i < n; i++) {
+        // if (i >= arr2.length) {
+        // arr[i] = 0;
+        // } else {
+        // arr[i] = arr2[i];
+        // }
+        // }
+
+        // OPTIMAL - O(n) time, no extra space
+        // find first zero
+        int i = -1;
+        for (int k = 0; k < n; k++) {
+            if (arr[k] == 0) {
+                i = k;
+                break;
             }
         }
 
+        // loop from i + 1, and keep swapping i with non zero numbers
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] > 0) {
+                arr[i] = arr[j];
+                arr[j] = 0;
+                i++;
+            }
+        }
     }
 
     public static void main(String[] args) {
