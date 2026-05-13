015def8d9d487ecd39e4e605388b55382f1ab7a6
Jayesh Singh <jayeshsingh001@gmail.com>
2026-05-13 21:57:29 +0530

    solve maximum consecutive ones - brute

---
+++ step3_arrays/lec_1_easy/MaximumConsecutiveOnes.java
@@ -0,0 +1,42 @@
+package step3_arrays.lec_1_easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MaximumConsecutiveOnes {
+
+    static int findMaxConsecutiveOnes(int[] arr) {
+
+        // -----------------------
+        // BRUTE - uses an additional array of size = number of 1 subarrays.
+        // TC - O(N log N)
+        // SC - O(C) where C = number of subarrays of 1s
+        List<Integer> l = new ArrayList<>();
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 1) {
+                count++;
+                if (i == arr.length - 1) {
+                    l.add(count);
+                }
+            } else {
+                if (count != 0) {
+                    l.add(count);
+                }
+                count = 0;
+            }
+        }
+
+        l.sort(null); // O(N log N)
+        return l.get(l.size() - 1);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 1, 1, 0, 0, 0, 1, 0,
+                1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1,
+                0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1 };
+
+        int n = findMaxConsecutiveOnes(arr);
+        System.out.println(n);
+    }
+}
