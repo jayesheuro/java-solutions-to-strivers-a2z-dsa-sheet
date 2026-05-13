fef5b8ba745caf296c0f225f1be931b5c59ee875
Jayesh Singh <jayeshsingh001@gmail.com>
2026-05-13 21:58:30 +0530

    add optimal solution for maximum consecutive ones

--- step3_arrays/lec_1_easy/MaximumConsecutiveOnes.java
+++ step3_arrays/lec_1_easy/MaximumConsecutiveOnes.java
@@ -7,28 +7,44 @@ public class MaximumConsecutiveOnes {
 
     static int findMaxConsecutiveOnes(int[] arr) {
 
-        // -----------------------
-        // BRUTE - uses an additional array of size = number of 1 subarrays.
-        // TC - O(N log N)
-        // SC - O(C) where C = number of subarrays of 1s
-        List<Integer> l = new ArrayList<>();
-        int count = 0;
-        for (int i = 0; i < arr.length; i++) {
+        // // -----------------------
+        // // BRUTE - uses an additional array of size = number of 1 subarrays.
+        // // TC - O(n) + O(C log C)
+        // // SC - O(C) where C = number of subarrays of 1s
+        // List<Integer> l = new ArrayList<>();
+        // int count = 0;
+        // for (int i = 0; i < arr.length; i++) {
+        // if (arr[i] == 1) {
+        // count++;
+        // if (i == arr.length - 1) {
+        // l.add(count);
+        // }
+        // } else {
+        // if (count != 0) {
+        // l.add(count);
+        // }
+        // count = 0;
+        // }
+        // }
+
+        // l.sort(null); // O(C log C)
+        // return l.get(l.size() - 1);
+
+        // OPTIMAL - uses O(1) extra space, and O(N) time complexity
+        int n = arr.length;
+        int k = 0; // current length of 1s subarray
+        int max = 0; // max consecutive 1s subarray length
+        for (int i = 0; i < n; i++) {
             if (arr[i] == 1) {
-                count++;
-                if (i == arr.length - 1) {
-                    l.add(count);
+                k++;
+                if (k > max) {
+                    max = k;
                 }
             } else {
-                if (count != 0) {
-                    l.add(count);
-                }
-                count = 0;
+                k = 0;
             }
         }
-
-        l.sort(null); // O(N log N)
-        return l.get(l.size() - 1);
+        return max;
     }
 
     public static void main(String[] args) {
