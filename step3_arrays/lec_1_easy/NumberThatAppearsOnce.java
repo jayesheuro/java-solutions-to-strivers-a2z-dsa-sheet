bb23d9a968901133a3ab8e288d1422b54c67cb55
Jayesh Singh <jayeshsingh001@gmail.com>
2026-05-14 23:32:52 +0530

    add optimal solution for number that appears once

--- step3_arrays/lec_1_easy/NumberThatAppearsOnce.java
+++ step3_arrays/lec_1_easy/NumberThatAppearsOnce.java
@@ -1,23 +1,29 @@
 package step3_arrays.lec_1_easy;
 
-import java.util.HashMap;
-import java.util.Map;
+// import java.util.HashMap;
+// import java.util.Map;
 
 public class NumberThatAppearsOnce {
 
     static int findNumberThatAppearsOnce(int[] arr) {
-        // BRUTE: O(n) time, O(n/2 +1) space
-        Map<Integer, Integer> m = new HashMap<>();
-        for (int i : arr) {
-            if (m.containsKey(i)) {
-                m.remove(i);
-            } else {
-                m.put(i, 1);
-            }
-        }
-        // at the end map will have only 1 key value pair
-        return m.keySet().iterator().next();
+        // // BRUTE: O(n) time, O(n/2 +1) space
+        // Map<Integer, Integer> m = new HashMap<>();
+        // for (int i : arr) {
+        // if (m.containsKey(i)) {
+        // m.remove(i);
+        // } else {
+        // m.put(i, 1);
+        // }
+        // }
+        // // at the end map will have only 1 key value pair
+        // return m.keySet().iterator().next();
 
+        // OPTIMAL use XOR, Time - O(N), Space - O(1)
+        int xor = 0;
+        for (int i : arr) {
+            xor ^= i;
+        }
+        return xor;
     }
 
     public static void main(String[] args) {
