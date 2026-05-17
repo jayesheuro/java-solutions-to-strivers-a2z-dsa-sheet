301b2f527ec13a962dc9204c34ce86076f4109d0
Jayesh Singh <jayeshsingh001@gmail.com>
2026-05-17 13:13:21 +0530

    add better solution for two sum

--- step3_arrays/lec_2_medium/TwoSum.java
+++ step3_arrays/lec_2_medium/TwoSum.java
@@ -1,15 +1,30 @@
 package step3_arrays.lec_2_medium;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class TwoSum {
     static boolean checkIfPairExists(int[] arr, int t) {
-        // version 1 - brute - O(n^2), O(1)
         boolean found = false;
+        // // version 1 - brute - O(n^2), O(1)
+        // for (int i = 0; i < arr.length; i++) {
+        // for (int j = i; j < arr.length; j++) {
+        // if (arr[i] + arr[j] == t) {
+        // found = true;
+        // break;
+        // }
+        // }
+        // }
+
+        // better - using hashmap
+        Map<Integer, Integer> arrMap = new HashMap<>();
         for (int i = 0; i < arr.length; i++) {
-            for (int j = i; j < arr.length; j++) {
-                if (arr[i] + arr[j] == t) {
-                    found = true;
-                    break;
-                }
+            if (arrMap.containsKey(t - arr[i])) {
+                found = true;
+                break;
+            } else {
+                arrMap.put(arr[i], i);
             }
         }
         return found;
@@ -17,7 +32,7 @@ public class TwoSum {
 
     public static void main(String[] args) {
         int[] arr = { 2, 6, 5, 8, 11 };
-        boolean found = checkIfPairExists(arr, 13);
+        boolean found = checkIfPairExists(arr, 19);
         if (found) {
             System.out.println("YES");
         } else {
