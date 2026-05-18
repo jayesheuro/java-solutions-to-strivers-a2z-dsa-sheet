47ac2a57f2ed003c6941883e2175968ff284b2a4
Jayesh Singh <jayeshsingh001@gmail.com>
2026-05-18 22:33:42 +0530

    add optimal solution for sort and array with zero one and two

--- step3_arrays/lec_2_medium/SortAnArrayOfZeroOneAndTwo.java
+++ step3_arrays/lec_2_medium/SortAnArrayOfZeroOneAndTwo.java
@@ -10,25 +10,50 @@ public class SortAnArrayOfZeroOneAndTwo {
         // // BRUTE - approach 1 LOL
         // Arrays.sort(arr); // O(N log N)
 
-        // BETTER - 2 Full Passes - O(N) + O(N)
-        int zeroes = 0, ones = 0, twos = 0;
-        for (int i : arr) {
-            if (i == 0) {
-                zeroes++;
-            } else if (i == 1) {
-                ones++;
-            } else {
-                twos++;
-            }
-        }
+        // // BETTER - 2 Full Passes - O(N) + O(N)
+        // int zeroes = 0, ones = 0, twos = 0;
+        // for (int i : arr) {
+        // if (i == 0) {
+        // zeroes++;
+        // } else if (i == 1) {
+        // ones++;
+        // } else {
+        // twos++;
+        // }
+        // }
 
-        for (int i = 0; i < n; i++) {
-            if (i < zeroes) {
-                arr[i] = 0;
-            } else if (i < zeroes + ones) {
-                arr[i] = 1;
+        // for (int i = 0; i < n; i++) {
+        // if (i < zeroes) {
+        // arr[i] = 0;
+        // } else if (i < zeroes + ones) {
+        // arr[i] = 1;
+        // } else {
+        // arr[i] = 2;
+        // }
+        // }
+
+        // OPTIMAL approach - DNG - pointer approach - think about 4 parts of array
+        // low: 0 to low will be zeroes
+        // mid: low to mid-1 will be ones
+        // mid to high - we will traverse
+        // high+1 to end - will have twos
+
+        int low = 0;
+        int mid = 0;
+        int high = n - 1;
+        while (mid <= high) {
+            if (arr[mid] == 0) {
+                arr[mid] = arr[low];
+                arr[low] = 0;
+                low++;
+                mid++;
+            } else if (arr[mid] == 1) {
+                // this means the element is already in between low and mid - 1
+                mid++;
             } else {
-                arr[i] = 2;
+                arr[mid] = arr[high];
+                arr[high] = 2;
+                high--;
             }
         }
     }
