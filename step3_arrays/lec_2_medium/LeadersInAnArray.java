14f031b7c0a23049c0eddce7adb0dbeb66a0f80a
Jayesh Singh <jayeshsingh001@gmail.com>
2026-06-02 22:39:29 +0530

    solve: leaders in an array optimal

--- step3_arrays/lec_2_medium/LeadersInAnArray.java
+++ step3_arrays/lec_2_medium/LeadersInAnArray.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class LeadersInAnArray {
+    static void getAllLeadersOnePass(int[] arr, List<Integer> ans) {
+
+    }
 
     static void getAllLeaders(int[] arr, List<Integer> ans) {
         for (int i = 0; i < arr.length; i++) {
@@ -25,7 +28,9 @@ public class LeadersInAnArray {
     public static void main(String[] args) {
         int[] arr = { 10, 22, 12, 3, 0, 6 };
         List<Integer> ans = new ArrayList<>();
+
         getAllLeaders(arr, ans);
+
         System.out.println(ans);
     }
 }
