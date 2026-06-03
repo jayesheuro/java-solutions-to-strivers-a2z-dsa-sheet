0eb9505f475227d6835e0443412203babf2160db
Jayesh Singh <jayeshsingh001@gmail.com>
2026-06-03 21:26:45 +0530

    solve getAllLeadersOnePass

--- step3_arrays/lec_2_medium/LeadersInAnArray.java
+++ step3_arrays/lec_2_medium/LeadersInAnArray.java
@@ -1,11 +1,27 @@
 package step3_arrays.lec_2_medium;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class LeadersInAnArray {
     static void getAllLeadersOnePass(int[] arr, List<Integer> ans) {
-
+        int max = Integer.MIN_VALUE;
+        for (int i = arr.length - 1; i >= 0; i--) {
+            if (arr[i] > max) {
+                ans.add(arr[i]);
+                max = arr[i];
+            }
+        }
+        // Collections.reverse(ans);
+        int i = 0, j = ans.size() - 1;
+        while (i < j) {
+            int temp = ans.get(i);
+            ans.set(i, ans.get(j));
+            ans.set(j, temp);
+            i++;
+            j--;
+        }
     }
 
     static void getAllLeaders(int[] arr, List<Integer> ans) {
@@ -29,7 +45,8 @@ public class LeadersInAnArray {
         int[] arr = { 10, 22, 12, 3, 0, 6 };
         List<Integer> ans = new ArrayList<>();
 
-        getAllLeaders(arr, ans);
+        // getAllLeaders(arr, ans);
+        getAllLeadersOnePass(arr, ans);
 
         System.out.println(ans);
     }
