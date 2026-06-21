8d5ef493f724df70070e24c00b7098a1dfade79f
Jayesh Singh <jayeshsingh001@gmail.com>
2026-06-21 10:44:50 +0530

    solve pascals triange returning list of list

--- step3_arrays/lec_3_hard/PascalsTriangle.java
+++ step3_arrays/lec_3_hard/PascalsTriangle.java
@@ -1,5 +1,8 @@
 package step3_arrays.lec_3_hard;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PascalsTriangle {
 
     static void printRow(int n) {
@@ -19,8 +22,31 @@ public class PascalsTriangle {
         }
     }
 
+    static List<Integer> getRow(int n) {
+        int ans = 1;
+        List<Integer> ansRow = new ArrayList<>();
+        ansRow.add(1);
+        for (int j = 1; j < n; j++) {
+            ans = ans * (n - j);
+            ans = ans / j;
+            ansRow.add(ans);
+        }
+        return ansRow;
+    }
+
+    static List<List<Integer>> getPascalTriangleList(int n) {
+        List<List<Integer>> ans = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            List<Integer> ansRow = getRow(i);
+            ans.add(ansRow);
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
         int n = 5;
-        printPascalTriangle(n);
+        // printPascalTriangle(n);
+        List<List<Integer>> ans = getPascalTriangleList(n);
+        System.out.println(ans);
     }
 }
