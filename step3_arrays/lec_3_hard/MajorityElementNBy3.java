503896bf0af4ba9124b152e7305b0aea5cc75a98
Jayesh Singh <jayeshsingh001@gmail.com>
2026-06-24 10:49:15 +0530

    solce majority element n by 3 better hashmap

--- step3_arrays/lec_3_hard/MajorityElementNBy3.java
+++ step3_arrays/lec_3_hard/MajorityElementNBy3.java
@@ -1,6 +1,10 @@
 package step3_arrays.lec_3_hard;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public class MajorityElementNBy3 {
@@ -10,6 +14,9 @@ public class MajorityElementNBy3 {
 
         for (int i = 0; i < arr.length; i++) {
             int count = 0;
+            if (ans.contains(arr[i])) {
+                continue; // skip checking again if value already > n/3 times and added to the set
+            }
             for (int j = 0; j < arr.length; j++) {
                 if (arr[j] == arr[i]) {
                     count++;
@@ -22,9 +29,24 @@ public class MajorityElementNBy3 {
         return ans;
     }
 
+    static List<Integer> findNby3MajorityElementBetter(int[] arr) {
+        int limit = arr.length / 3;
+        List<Integer> ans = new ArrayList<>();
+        // count frequencies
+        Map<Integer, Integer> fm = new HashMap<>();
+        for (int i : arr) {
+            fm.put(i, fm.getOrDefault(i, 0) + 1);
+            if (fm.get(i) == limit + 1) {
+                ans.add(i);
+            }
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
         int[] arr = { 1, 2, 1, 1, 3, 2, 2 };
-        Set<Integer> ans = findNby3MajorityElement(arr);
+        // Set<Integer> ans = findNby3MajorityElement(arr);
+        List<Integer> ans = findNby3MajorityElementBetter(arr);
         System.out.println(ans);
     }
 }
