352145cbad7733f61e9216df66bdc7865bb614bb
Jayesh Singh <jayeshsingh001@gmail.com>
2026-07-10 16:19:42 +0530

    typo

--- step3_arrays/lec_3_hard/BackToBasics.java
+++ step3_arrays/lec_3_hard/BackToBasics.java
@@ -2,6 +2,6 @@ package step3_arrays.lec_3_hard;
 
 public class BackToBasics {
     public static void main(String[] args) {
-        System.out.println("Vacation over, now back to basics. Lets grill!");
+        System.out.println("Vacation over, now back to basics. Lets grind!");
     }
 }
