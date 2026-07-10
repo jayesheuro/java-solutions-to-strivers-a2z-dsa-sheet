e025146e3b04888e3a0285303c3e0586036a09e9
Jayesh Singh <jayeshsingh001@gmail.com>
2026-07-10 16:19:23 +0530

    we are back to basics

---
+++ step3_arrays/lec_3_hard/BackToBasics.java
@@ -0,0 +1,7 @@
+package step3_arrays.lec_3_hard;
+
+public class BackToBasics {
+    public static void main(String[] args) {
+        System.out.println("Vacation over, now back to basics. Lets grill!");
+    }
+}
