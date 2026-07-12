49fcfdcf0454866cf0cdd2e8329bab71bb4aee36
Jayesh Singh <jayeshsingh001@gmail.com>
2026-07-12 10:07:13 +0530

    add better solution for threeSum

--- step3_arrays/lec_3_hard/ThreeSum.java
+++ step3_arrays/lec_3_hard/ThreeSum.java
@@ -6,8 +6,7 @@ import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-// import java.util.stream.Collector;
-// import java.util.stream.Collectors;
+import java.util.stream.Collectors;
 
 public class ThreeSum {
     static List<List<Integer>> threeSum(int[] arr) {
@@ -24,14 +23,32 @@ public class ThreeSum {
             }
         }
         List<List<Integer>> ans = new ArrayList<>(uniqueTriplets);
-        // List<List<Integer>> ans =
-        // uniqueTriplets.stream().collect(Collectors.toList());
         return ans;
+    }
+
+    static List<List<Integer>> threeSumBetter(int[] arr) {
+        Set<List<Integer>> uniqueTriplets = new HashSet<>();
+        Set<Integer> visited = new HashSet<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            visited.clear();
+            for (int j = i + 1; j < arr.length; j++) {
+                int third = -(arr[i] + arr[j]);
+                if (visited.contains(third)) {
+                    // we found a triplet
+                    List<Integer> triplet = Arrays.asList(arr[i], arr[j], third);
+                    triplet.sort(Comparator.naturalOrder());
+                    uniqueTriplets.add(triplet);
+                }
+                visited.add(arr[j]);
+            }
+        }
+        return uniqueTriplets.stream().collect(Collectors.toList());
     }
 
     public static void main(String[] args) {
         int[] arr = { -1, 0, 1, 2, -1, -4 };
-        List<List<Integer>> ans = threeSum(arr);
+        List<List<Integer>> ans = threeSumBetter(arr);
         System.out.println(ans);
     }
 }
