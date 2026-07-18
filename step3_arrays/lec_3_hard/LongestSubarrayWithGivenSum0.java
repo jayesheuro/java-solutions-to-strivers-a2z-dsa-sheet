0726640e79933e9ded27902aa36455cf9eed967d
Jayesh Singh <jayeshsingh001@gmail.com>
2026-07-18 22:47:02 +0530

    solve findMaxSubArrayLengthSum0 brute

---
+++ step3_arrays/lec_3_hard/LongestSubarrayWithGivenSum0.java
@@ -0,0 +1,27 @@
+package step3_arrays.lec_3_hard;
+
+public class LongestSubarrayWithGivenSum0 {
+
+    static int findMaxSubArrayLengthSum0(int[] arr, int k) {
+        // BRUTE
+        int maxLength = 0;
+        int currentSum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            currentSum = 0;
+            for (int j = i; j < arr.length; j++) {
+                currentSum += arr[j];
+                if (currentSum == k) {
+                    int l = ((j - i) + 1);
+                    maxLength = Math.max(maxLength, l);
+                }
+            }
+        }
+        return maxLength;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 9, -3, 3, -1, 6, -5 };
+        int len = findMaxSubArrayLengthSum0(arr, 0);
+        System.out.println(len);
+    }
+}
