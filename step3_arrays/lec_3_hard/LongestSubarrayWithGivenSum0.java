478019b46acc8c5d40b90e386dcc71ca8613befa
Jayesh Singh <jayeshsingh001@gmail.com>
2026-08-07 21:50:22 +0530

    solve longest subarray sum k optimal

--- step3_arrays/lec_3_hard/LongestSubarrayWithGivenSum0.java
+++ step3_arrays/lec_3_hard/LongestSubarrayWithGivenSum0.java
@@ -1,5 +1,8 @@
 package step3_arrays.lec_3_hard;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class LongestSubarrayWithGivenSum0 {
 
     static int findMaxSubArrayLengthSum0(int[] arr, int k) {
@@ -19,9 +22,30 @@ public class LongestSubarrayWithGivenSum0 {
         return maxLength;
     }
 
+    static int findMaxSubArrayLengthSum0Optimal(int[] arr, int k) {
+        int maxLength = 0;
+        int currentSum = 0;
+
+        Map<Integer, Integer> sumTracker = new HashMap<>();
+        sumTracker.put(0, -1);
+
+        for (int i = 0; i < arr.length; i++) {
+            currentSum += arr[i];
+            int remainder = currentSum - k;
+            if (sumTracker.containsKey(remainder)) {
+                // means we found a subarray with sum k
+                int l = i - sumTracker.get(remainder);
+                maxLength = Math.max(l, maxLength);
+            } else {
+                sumTracker.put(currentSum, i);
+            }
+        }
+        return maxLength;
+    }
+
     public static void main(String[] args) {
         int[] arr = { 9, -3, 3, -1, 6, -5 };
-        int len = findMaxSubArrayLengthSum0(arr, 0);
+        int len = findMaxSubArrayLengthSum0Optimal(arr, 0);
         System.out.println(len);
     }
 }
