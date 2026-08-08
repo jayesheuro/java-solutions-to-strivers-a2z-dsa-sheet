5a60ac4d891ae0e6e99d7c024ae2ccb6b9f39695
Jayesh Singh <jayeshsingh001@gmail.com>
2026-08-08 22:08:43 +0530

    solve count subarrays with given xor brute

---
+++ step3_arrays/lec_3_hard/CountSubarraysWithGivenXORk.java
@@ -0,0 +1,26 @@
+package step3_arrays.lec_3_hard;
+
+public class CountSubarraysWithGivenXORk {
+    static int countSubarrayWithGivenXorK(int[] arr, int k) {
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            int currentXor = arr[i];
+            if (currentXor == k) {
+                count++;
+            }
+            for (int j = i + 1; j < arr.length; j++) {
+                currentXor = currentXor ^ arr[j];
+                if (currentXor == k) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 5, 6, 7, 8, 9 };
+        int k = 6;
+        System.out.println(countSubarrayWithGivenXorK(arr, k));
+    }
+}
