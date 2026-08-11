afadb0146de5c9664aaf4feb26d846475828cad6
Jayesh Singh <jayeshsingh001@gmail.com>
2026-08-11 22:50:03 +0530

    solve merge overlapping intervals brute

---
+++ step3_arrays/lec_3_hard/MergeOverlappingIntervals.java
@@ -0,0 +1,31 @@
+package step3_arrays.lec_3_hard;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MergeOverlappingIntervals {
+    static List<List<Integer>> mergeOverlapping(int[][] arr) {
+        List<List<Integer>> list = new ArrayList<>();
+        if (arr.length == 1) {
+            list.add(Arrays.asList(arr[0][0], arr[0][1]));
+            return list;
+        }
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i][1] >= arr[i + 1][0]) {
+                // merge
+                list.add(Arrays.asList(arr[i][0], arr[i + 1][1]));
+            } else {
+                list.add(Arrays.asList(arr[i + 1][0], arr[i + 1][1]));
+            }
+        }
+        return list;
+    }
+
+    public static void main(String[] args) {
+        // int[][] arr = { { 1, 3 }, { 2, 6 }, { 8, 10 }, { 15, 18 } };
+        int[][] arr = { { 1, 4 }, { 4, 5 } };
+        System.out.println(mergeOverlapping(arr));
+    }
+}
