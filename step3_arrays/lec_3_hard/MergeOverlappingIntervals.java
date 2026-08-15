3aa4ba8bbfc104dda208ebbe913d5e0ddd3d4636
Jayesh Singh <jayeshsingh001@gmail.com>
2026-08-15 20:58:58 +0530

    solve merge overlapping intervals optimal

--- step3_arrays/lec_3_hard/MergeOverlappingIntervals.java
+++ step3_arrays/lec_3_hard/MergeOverlappingIntervals.java
@@ -6,29 +6,52 @@ import java.util.List;
 
 public class MergeOverlappingIntervals {
 
-    // this does not work on unsorted intervals
-    // this gives incorrect answer for cases like (1,4) (2,5) (3,6)
-    static List<List<Integer>> mergeOverlapping(int[][] arr) {
-        List<List<Integer>> list = new ArrayList<>();
-        if (arr.length == 1) {
-            list.add(Arrays.asList(arr[0][0], arr[0][1]));
-            return list;
-        }
+    static int[][] mergeOverlapping(int[][] intervals) {
+        List<int[]> ans = new ArrayList<>();
+        // sorting the array lexicographically
+        Arrays.sort(intervals, (a, b) -> {
+            if (a[0] != b[0]) {
+                return Integer.compare(a[0], b[0]);
+            }
+            return Integer.compare(a[1], b[1]);
+        });
 
-        for (int i = 0; i < arr.length - 1; i++) {
-            if (arr[i][1] >= arr[i + 1][0]) {
-                // merge
-                list.add(Arrays.asList(arr[i][0], arr[i + 1][1]));
+        int n = intervals.length;
+
+        // adding first element
+        ans.add(new int[] {
+                intervals[0][0],
+                intervals[0][1]
+        });
+
+        for (int i = 1; i < n; i++) {
+            int currentStart = intervals[i][0];
+            int currentEnd = intervals[i][1];
+
+            int[] last = ans.get(ans.size() - 1); // reference to last merged interval
+
+            if (currentStart <= last[1]) {
+                // overlapping
+                last[1] = Math.max(currentEnd, last[1]);
             } else {
-                list.add(Arrays.asList(arr[i + 1][0], arr[i + 1][1]));
+                ans.add(new int[] { currentStart, currentEnd });
             }
         }
-        return list;
+
+        // convert List<int[]> into int[][]
+        return ans.toArray(new int[ans.size()][]);
     }
 
     public static void main(String[] args) {
-        // int[][] arr = { { 1, 3 }, { 2, 6 }, { 8, 10 }, { 15, 18 } };
-        int[][] arr = { { 1, 4 }, { 4, 5 } };
-        System.out.println(mergeOverlapping(arr));
+        int[][] intervals = { { 2, 6 }, { 1, 3 }, { 15, 18 }, { 8, 10 } };
+        // int[][] intervals = { { 1, 4 }, { 4, 5 } };
+        int[][] overlapped = mergeOverlapping(intervals);
+        for (int[] a : overlapped) {
+            for (int i : a) {
+                System.out.print(i + ", ");
+            }
+            System.out.println();
+        }
+        // System.out.println(mergeOverlapping(intervals));
     }
 }
