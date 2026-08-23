f7c23255dd6b6659a0eea795b9034045d14e5414
Jayesh Singh <jayeshsingh001@gmail.com>
2026-08-23 20:51:51 +0530

    solve reverse pairs brute force

---
+++ step3_arrays/lec_3_hard/ReversePairs.java
@@ -0,0 +1,22 @@
+package step3_arrays.lec_3_hard;
+
+public class ReversePairs {
+    static int countReversePairs(int[] arr) {
+        int count = 0;
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] > 2 * arr[j]) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        // int[] arr = { 1, 3, 2, 3, 1 };
+        int[] arr = { 3, 2, 1, 4 };
+        System.out.println(countReversePairs(arr));
+    }
+}
