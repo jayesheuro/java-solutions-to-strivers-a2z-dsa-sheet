81a91b153bf64bfc574e564746f0c6e4a9444b7d
Jayesh Singh <jayeshsingh001@gmail.com>
2026-08-25 22:50:35 +0530

    solve max product subarray brute

---
+++ step3_arrays/lec_3_hard/MaximumProductSubarray.java
@@ -0,0 +1,28 @@
+package step3_arrays.lec_3_hard;
+
+public class MaximumProductSubarray {
+    static int mps(int[] arr) {
+        if (arr.length <= 1) {
+            return arr[0];
+        }
+        if (arr.length == 0) {
+            return 0;
+        }
+        int maxProduct = arr[0];
+        int currentProduct = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            currentProduct = arr[i];
+            for (int j = i + 1; j < arr.length; j++) {
+                currentProduct *= arr[j];
+                maxProduct = Math.max(currentProduct, maxProduct);
+            }
+        }
+        return maxProduct;
+    }
+
+    public static void main(String[] args) {
+        // int[] arr = { 1, 2, -3, 0, -4, -5 };
+        int[] arr = { 1, 2, 3, -4, -5, 1, 0, -8 };
+        System.out.println(mps(arr));
+    }
+}
