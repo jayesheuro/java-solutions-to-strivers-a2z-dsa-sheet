605c87114441f5cfe15e75b79d5e75fbcf66db44
Jayesh Singh <jayeshsingh001@gmail.com>
2026-08-25 22:41:03 +0530

    removed useless comment

--- step3_arrays/lec_3_hard/ReversePairs.java
+++ step3_arrays/lec_3_hard/ReversePairs.java
@@ -76,7 +76,6 @@ public class ReversePairs {
 
     public static void main(String[] args) {
         int[] arr = { 1, 3, 2, 3, 1 };
-        // int[] arr = { 3, 2, 1, 4 };
         System.out.println(countReversePairsOptimal(arr, arr.length));
     }
 }
