f8e446853e297ad9fbede7c564d569b556cf6011
Jayesh Singh <jayeshsingh001@gmail.com>
2026-08-29 20:50:32 +0530

    correct solution for lower bound algorithm

--- step4_binary_search/bs_on_1d_arrays/LowerBound.java
+++ step4_binary_search/bs_on_1d_arrays/LowerBound.java
@@ -12,10 +12,47 @@ public class LowerBound {
         return pos;
     }
 
+    // static int lowerBound(int[] arr, int low, int high, int target) {
+    // int mid = low + ((high - low) / 2);
+    // if (low >= high) {
+    // return low;
+    // }
+    // if (arr[mid] == target) {
+    // return mid;
+    // } else if (arr[mid] < target) {
+    // return lowerBound(arr, mid + 1, high, target);
+    // }
+    // return lowerBound(arr, low, mid - 1, target);
+    // }
+
+    // static int findLowerBoundOptimal(int[] arr, int target, int n) {
+
+    // int pos = lowerBound(arr, 0, n - 1, target);
+    // return pos;
+    // }
+
+    static int findLowerBoundOptimal(int[] arr, int target, int n) {
+        int ans = n;
+        int low = 0;
+        int high = n - 1;
+
+        while (low <= high) {
+            int mid = low + ((high - low) / 2);
+            if (arr[mid] >= target) {
+                // probable answer
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
-        int[] arr = { 1, 3, 5, 7, 23, 67, 88, 100 };
-        int x = 88;
-        int lowerBound = findLowerBound(arr, x, arr.length);
+        int[] arr = { 1, 3, 5, 7, 23, 67, 88, 100, 200 };
+        int x = 600;
+        int lowerBound = findLowerBoundOptimal(arr, x, arr.length);
         System.out.println(lowerBound);
     }
 }
