fa6df0cd450562c9e9454d78f7367af67b49a805
Jayesh Singh <jayeshsingh001@gmail.com>
2026-08-30 17:47:59 +0530

    solve upper bound optimal

--- step4_binary_search/bs_on_1d_arrays/UpperBound.java
+++ step4_binary_search/bs_on_1d_arrays/UpperBound.java
@@ -3,10 +3,23 @@ package step4_binary_search.bs_on_1d_arrays;
 public class UpperBound {
     static int upperBound(int[] arr, int n, int x) {
         int ans = n;
-        for (int i = 0; i < n; i++) {
-            if (arr[i] > x) {
-                ans = i;
-                break;
+        // for (int i = 0; i < n; i++) {
+        // if (arr[i] > x) {
+        // ans = i;
+        // break;
+        // }
+        // }
+
+        int low = 0;
+        int high = n - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] > x) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
             }
         }
 
