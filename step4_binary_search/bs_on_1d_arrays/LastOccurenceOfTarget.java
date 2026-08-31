91d808baf779dbd1f40a767f4785727fd31088e9
Jayesh Singh <jayeshsingh001@gmail.com>
2026-08-31 23:27:53 +0530

    solve last occurence of target brute

---
+++ step4_binary_search/bs_on_1d_arrays/LastOccurenceOfTarget.java
@@ -0,0 +1,17 @@
+package step4_binary_search.bs_on_1d_arrays;
+
+public class LastOccurenceOfTarget {
+    static int lastOccurenceOfTarget(int[] arr, int n, int target) {
+        for (int i = n - 1; i >= 0; i--) {
+            if (arr[i] == target)
+                return i;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 3, 4, 13, 13, 13, 20, 40, 3, 4, 13, 13, 13, 20, 40 };
+        int n = arr.length;
+        System.out.println(lastOccurenceOfTarget(arr, n, 13));
+    }
+}
