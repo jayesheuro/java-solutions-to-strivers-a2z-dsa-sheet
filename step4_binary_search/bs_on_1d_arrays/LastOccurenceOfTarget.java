a077564418f6885e08146484abe475988a72705a
Jayesh Singh <jayeshsingh001@gmail.com>
2026-09-06 21:56:49 +0530

    corrected solution removing loops

--- step4_binary_search/bs_on_1d_arrays/LastOccurenceOfTarget.java
+++ step4_binary_search/bs_on_1d_arrays/LastOccurenceOfTarget.java
@@ -10,7 +10,7 @@ public class LastOccurenceOfTarget {
         return -1;
     }
 
-    static int lastOccurenceOfTargetBetter(int[] arr, int n, int target) {
+    static int lastOccurenceOfTargetOptimal(int[] arr, int n, int target) {
         int high = n - 1;
         int low = 0;
         int pos = -1;
@@ -18,26 +18,27 @@ public class LastOccurenceOfTarget {
             int mid = low + high - low / 2;
             if (arr[mid] == target) {
                 pos = mid;
-                break;
+                // break;
+                low = mid + 1;
             } else if (arr[mid] < target) {
                 low = mid + 1;
             } else {
                 high = mid - 1;
             }
         }
-        if (pos != -1) {
-            for (int i = pos; i < n - 1; i++) {
-                if (arr[i] != target) {
-                    return pos;
-                }
-            }
-        }
+        // if (pos != -1) {
+        // for (int i = pos; i < n - 1; i++) {
+        // if (arr[i] != target) {
+        // return pos;
+        // }
+        // }
+        // }
         return pos;
     }
 
     public static void main(String[] args) {
         int[] arr = { 3, 4, 13, 13, 13, 20, 40, 66 };
         int n = arr.length;
-        System.out.println(lastOccurenceOfTargetBetter(arr, n, 13));
+        System.out.println(lastOccurenceOfTargetOptimal(arr, n, 13));
     }
 }
