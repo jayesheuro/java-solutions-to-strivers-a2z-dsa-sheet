bc87da18728b9a5d43afbabc39dd788a5af00d52
Jayesh Singh <jayeshsingh001@gmail.com>
2026-09-10 21:41:26 +0530

    chore:comment

--- step4_binary_search/bs_on_1d_arrays/CountOccurencesInSortedArray.java
+++ step4_binary_search/bs_on_1d_arrays/CountOccurencesInSortedArray.java
@@ -1,6 +1,7 @@
 package step4_binary_search.bs_on_1d_arrays;
 
 public class CountOccurencesInSortedArray {
+    //brute
     static int countOccurences(int[]arr, int target){
         int ans= 0;
         for(int i: arr){
