269ec42ed90a242847f4ec1510375c1e03d19994
Jayesh Singh <jayeshsingh001@gmail.com>
2026-09-12 19:02:47 +0530

    add optimal solution for count occurences in sorted array

--- step4_binary_search/bs_on_1d_arrays/CountOccurencesInSortedArray.java
+++ step4_binary_search/bs_on_1d_arrays/CountOccurencesInSortedArray.java
@@ -8,8 +8,53 @@ public class CountOccurencesInSortedArray {
         }
         return ans;
     }
+    static int countOccurencesOptimal(int[] arr, int target) {
+
+        int n = arr.length;
+
+        int low = 0;
+        int high = n - 1;
+
+        int ub = n;
+        int lb = n;
+
+        // Upper Bound
+        while (low <= high) {
+
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] > target) {
+                ub = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        // Lower Bound
+        low = 0;
+        high = n - 1;
+
+        while (low <= high) {
+
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] >= target) {
+                lb = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        if (lb == n || arr[lb] != target) {
+            return 0;
+        }
+
+        return ub - lb;
+    }
     public static void main(String[] args) {
         int[]arr = {2, 2 , 3 , 3 , 3 , 3 , 4};
-        System.out.println(countOccurences(arr, 3));
+        System.out.println(countOccurencesOptimal(arr, 3));
     }
 }
