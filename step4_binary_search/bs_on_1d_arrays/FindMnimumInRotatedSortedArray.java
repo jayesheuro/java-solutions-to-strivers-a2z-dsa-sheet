911be867a884d88b13a79ab0db86877430c925f1
Jayesh Singh <jayeshsingh001@gmail.com>
2026-09-19 15:38:30 +0530

    solve optimally - finding minimum in sorted array

--- step4_binary_search/bs_on_1d_arrays/FindMnimumInRotatedSortedArray.java
+++ step4_binary_search/bs_on_1d_arrays/FindMnimumInRotatedSortedArray.java
@@ -9,8 +9,47 @@ public class FindMnimumInRotatedSortedArray {
         }
         return min;
     }
+
+    static int findMinBetter(int[]arr){
+        if (arr.length == 0) return -1;
+        int min = arr[0];
+        for(int i = 0; i < arr.length-1; i++){
+            System.out.print(i);
+            if(arr[i+1] < arr[i]){
+                min = arr[i + 1];
+            }
+        }
+        return min;
+    }
+
+    static int findMinOptimal(int[]arr){
+        if (arr.length == 0) return -1;
+        int min = Integer.MAX_VALUE;
+        int low = 0, high = arr.length - 1;
+        
+        while(low <= high) {
+            int mid = low + ((high - low) / 2);
+            min = Math.min(min, arr[mid]);
+            if(arr[low] <= arr[mid]){
+                // left half is sorted
+                // lowest element is arr[low]
+                min = Math.min(min, arr[low]);
+                // eliminate left half now
+                low = mid + 1;
+            } else {
+                // right half is sorted
+                // arr[mid] should be smallest in the right sorted half
+                min = Math.min(min, arr[mid]);
+                // eliminate the right half
+                high = mid - 1;
+            }
+        }
+        return min;
+    }
+    
     public static void main(String[] args) {
-        int[]arr = {4,5,6,7,0,1,2,3};
-        System.out.println(findMin(arr));
+        // System.out.println(findMinOptimal(new int[]{1,2,3,4,5,6,7,0}));
+        System.out.println(findMinOptimal(new int[]{4,5,6,7,0,1,2,3}));
+        // System.out.println(findMinOptimal(new int[]{6,7,0,1,2,3,4,5}));
     }
 }
