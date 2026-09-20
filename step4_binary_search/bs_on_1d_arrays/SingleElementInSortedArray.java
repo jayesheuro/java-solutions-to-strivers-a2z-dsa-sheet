06a8400f0bd7c4ec73e199bde61f2178e2f5ac19
Jayesh Singh <jayeshsingh001@gmail.com>
2026-09-20 23:07:33 +0530

    add brute - 2 xor for single element in sorted array

--- step4_binary_search/bs_on_1d_arrays/SingleElementInSortedArray.java
+++ step4_binary_search/bs_on_1d_arrays/SingleElementInSortedArray.java
@@ -5,14 +5,21 @@ public class SingleElementInSortedArray {
         int ans = -1;
         if(n == 0) return ans;
         
-        //elements should repeat on odd positions, and increase on even positions as per zero based indexing
+        // //elements should repeat on odd positions, and increase on even positions as per zero based indexing
+        // for(int i = 0; i < n; i++){
+        //     if((i==n-1) || (i % 2 == 0 && arr[i+1] != arr[i])) {
+        //         return arr[i];
+        //     }
+        // }
+        // return ans;
+
+        // O(N) using xor
+        int xor = 0;
         for(int i = 0; i < n; i++){
-            if((i==n-1) || (i % 2 == 0 && arr[i+1] != arr[i])) {
-                return arr[i];
-            }
+            xor = xor ^ arr[i];
         }
 
-        return ans;
+        return xor;
     }
     public static void main(String[] args) {
         int[]arr = {1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7};
