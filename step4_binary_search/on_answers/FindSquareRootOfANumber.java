d3b92188d2b971bdbc2ac1b08839a60b164fe96a
Jayesh Singh <jayeshsingh001@gmail.com>
2026-09-23 19:44:03 +0530

    add optimal solve for finding square root of a number

--- step4_binary_search/on_answers/FindSquareRootOfANumber.java
+++ step4_binary_search/on_answers/FindSquareRootOfANumber.java
@@ -6,8 +6,25 @@ public class FindSquareRootOfANumber {
         return (int) Math.sqrt(n);
     }
 
+    static int findSquareRootOptimal(int n){
+        int low = 0;
+        int high = n;
+        int ans = 0;
+        while(low <= high){
+            int mid = low + ((high - low)/2);
+            if(mid <= n / mid){
+                // mid is a possible square root
+                ans = mid;
+                low = mid+1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
         int n = 25;
-        System.out.println(findSquareRoot(n));
+        System.out.println(findSquareRootOptimal(n));
     }
 }
